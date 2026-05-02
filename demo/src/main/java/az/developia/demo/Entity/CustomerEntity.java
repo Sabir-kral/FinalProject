bd48363293e3094b39e25830f094d69300a1c3c2
sabir kral <sabirmemmedli21152014@gmail.com>
2026-05-02 16:10:12 +0400

    asd

---
+++ demo/src/main/java/az/developia/demo/Entity/CustomerEntity.java
@@ -0,0 +1,17 @@
+package az.developia.demo.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerEntity {
+    private Long id;
+
+}
